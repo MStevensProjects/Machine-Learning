e8c32e7b8e312ce565bb2271d6a8869f969f9f22
Matthew Stevens <matt.tyler.stevens@gmail.com>
2022-04-12 00:55:30 -0400

    Updated import statement

--- Simple Linear Regression/simple_linear_regression.py
+++ Simple Linear Regression/simple_linear_regression.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import scikit-learn as sklearn
+import sklearn as sklearn
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
